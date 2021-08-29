93eccb8396b34a2c12b03ad362bad36dd4632fb6
valeraron <77055501+valeraron@users.noreply.github.com>
2021-08-29 03:50:49 +0300

    добавлена разметка для второго задания

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import { ProfileList } from 'components/Profile/ProfileList';
+import { Statistics } from './Statistics/Statistics';
 import user from '../user.json';
+import statisticalData from '../statistical-data.json';
 
 export default function App() {
   return (
     <div>
       <ProfileList user={user} />
+      <Statistics title="Upload stats" stats={statisticalData} />;
     </div>
   );
 }
